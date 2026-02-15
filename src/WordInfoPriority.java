580c7d347ddea885f98ad77f647b7992c290a5de
Joshua Taggart <jftaggart02@gmail.com>
2026-02-14 19:17:30 -0700

    inherit from wordinfo

--- src/WordInfoPriority.java
+++ src/WordInfoPriority.java
@@ -1,29 +1,13 @@
-public class WordInfoPriority implements Comparable<WordInfoPriority> {
+public class WordInfoPriority extends WordInfo implements Comparable<WordInfoPriority> {
 
     public WordInfoPriority(String word, int moves, int estimatedWork) {
-        this.word = word;
-        this.moves = moves;
-        this.priority = moves + estimatedWork;
-        this.history = word;
+        super(word, moves);
+        this.priority = estimatedWork + moves;
     }
 
     public WordInfoPriority(String word, int moves, int estimatedWork, String history) {
-        this.word = word;
-        this.moves = moves;
-        this.priority = moves + estimatedWork;
-        this.history = history;
-    }
-
-    public String getWord() {
-        return this.word;
-    }
-
-    public int getMoves() {
-        return this.moves;
-    }
-
-    public String getHistory() {
-        return this.history;
+        super(word, moves, history);
+        this.priority = estimatedWork + moves;
     }
 
     public int getPriority() { return this.priority; }
@@ -34,8 +18,5 @@ public class WordInfoPriority implements Comparable<WordInfoPriority> {
     }
 
     private int priority;
-    private String word;
-    private int moves;
-    private String history;
 
 }
